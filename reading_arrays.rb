cf22c99fb79af0b942c4c227b3e68cb51f54e60d
Jayden Sansom <jaydensansom@gmail.com>
2018-07-25 18:43:11 +0000

    Done.

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,25 +8,37 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  #Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES[0]
 end
+
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  #lmao its famlit famalamadingdong
+  STUDENT_NAMES[3]
 end
+
 
 def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  #sup brah
+  STUDENT_NAMES[5]
 end
+
 
 def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+  #hi
+  STUDENT_NAMES.push.first
 end
+
 
 def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+  #famlit
+  STUDENT_NAMES.push.last
 end
 
+
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+  #yyyaaaaassss
+  [STUDENT_NAMES[0],STUDENT_NAMES[1],STUDENT_NAMES[2]]
 end
+
